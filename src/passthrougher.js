07d950eab9826de167fa0608e489b80410b3eb41
Ben Bradley <[ben.bradley@cigna.com|mailto:ben.bradley@cigna.com]>
2016-05-06 08:42:23 -0700

    adding first & last methods

--- src/passthrougher.js
+++ src/passthrougher.js
@@ -7,18 +7,58 @@ const passthrougher = () => ({
 
   tap(cb) {
     let { objectMode } = this,
-      tap = new PassThrough({ objectMode });
+      stream = new PassThrough({ objectMode });
 
     makeSure(cb).is.a.Function;
 
-    tap.on('data', (data) => {
+    stream.on('data', (data) => {
       if (objectMode)
         cb(data);
       else if (!objectMode)
         cb(data.toString());
     });
 
-    return this.pipe(tap);
+    return this.pipe(stream);
+  },
+
+  first(cb) {
+    let { objectMode } = this,
+      stream = new PassThrough({ objectMode }),
+      done = false;
+
+    makeSure(cb).is.a.Function;
+
+    stream.on('data', (data) => {
+      if (done)
+        return;
+
+      done = true;
+      if (objectMode)
+        cb(data);
+      else if (!objectMode)
+        cb(data.toString());
+    });
+
+    return this.pipe(stream);
+  },
+
+  last(cb) {
+    let { objectMode } = this,
+      stream = new PassThrough({ objectMode }),
+      last = {};
+
+    makeSure(cb).is.a.Function;
+
+    stream.on('data', (data) => last = data);
+
+    stream.on('finish', () => {
+      if (objectMode)
+        cb(last);
+      else if (!objectMode)
+        cb(last.toString());
+    });
+
+    return this.pipe(stream);
   }
 
 });

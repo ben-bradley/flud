84797619d826f41e6d0708d7042adcc882d3bd90
Ben Bradley <[ben.bradley@cigna.com|mailto:ben.bradley@cigna.com]>
2016-05-06 08:43:06 -0700

    adding tests

--- dist/test/passthrougher.js
+++ dist/test/passthrougher.js
@@ -36,7 +36,45 @@ var passthrougherTests = function passthrougherTests(inputs) {
           done();
         });
       });
-    });
+    }); // end tap tests
+
+    describe('first', function () {
+
+      it('should call fn with only the first value', function (done) {
+        var _inputs2 = inputs();
+
+        var objects = _inputs2.objects;
+        var flud = new _2.default(objects);
+        var first = {};
+
+        flud.first(function (data) {
+          return first = data;
+        }).stream.on('end', function () {
+          first.should.be.an.Object.with.property('a');
+          first.a.should.eql(objects[0].a);
+          done();
+        });
+      });
+    }); // end first tests
+
+    describe('last', function () {
+
+      it('should call fn with only the last value', function (done) {
+        var _inputs3 = inputs();
+
+        var objects = _inputs3.objects;
+        var flud = new _2.default(objects);
+        var last = {};
+
+        flud.last(function (data) {
+          return last = data;
+        }).stream.on('end', function () {
+          last.should.be.an.Object.with.property('a');
+          last.a.should.eql(objects[objects.length - 1].a);
+          done();
+        });
+      });
+    }); // end last tests
   }); // end passthrougher tests
 };
 
